40062e165c1fa76120921c8c7dc48d78fb3f00ac
hotdog <hotdog929@gmail.com>
2017-06-12 12:43:26 +0800

    修改 改呼叫正式機的 deploy api

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -X POST -H 'Content-Type: application/json' -d "{\"Repository\":\"107highscrev\",\"Tag\":\"${TRAVIS_TAG}\",\"Owner\":\"eHanlin\",\"Password\":\"${EHANLIN_PW}\"}" 'http://test.ehanlin.com.tw/event/api/Deploy'
+curl -X POST -H 'Content-Type: application/json' -d "{\"Repository\":\"107highscrev\",\"Tag\":\"${TRAVIS_TAG}\",\"Owner\":\"eHanlin\",\"Password\":\"${EHANLIN_PW}\"}" 'http://api.ehanlin.com.tw/event/api/Deploy'
